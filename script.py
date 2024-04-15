115cba8fc82b656f8a64580896085d3b1dfc2e1c
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-04-15 20:31:21 +0100

    Akkadian now works with nvda

--- script.py
+++ script.py
@@ -16,10 +16,10 @@ if option == 1:
 elif option==2:
     print("adding symbols to nvda")
     akkadian=pd.read_csv("languages/filtered_akkadian.csv")
-    nvda_symbols_file=open("C:/Program Files (x86)/NVDA/locale/en/symbols.dic","a+")
+    nvda_symbols_file=open("C:/Program Files (x86)/NVDA/locale/en/symbols.dic","a+",encoding="utf-8")
     nvda_symbols_file.write("\n#Beta Akkadian")
     for index,row in akkadian.iterrows():
-        new_line=str(row["Character(decimal)"].encode("utf-8"))+"\t"+row["Name"]+"\tmost\talways"
+        new_line=str(row["Character(decimal)"])+"\t"+str(row["Name"])+"\tmost\talways\n"
         nvda_symbols_file.write(new_line)
     nvda_symbols_file.write("#End Beta Akkadian")
     nvda_symbols_file.close()
