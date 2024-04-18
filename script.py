1c29f61bb5dc038d945f5437a9740d07cebdb7d6
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-04-18 14:55:29 +0300

    Added code to replace dbt symbols with regular ueb symbols.

--- script.py
+++ script.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import json
 
+braille_file=open("utils/brailleconverter.json")
+braille_object=json.load(braille_file)
+
+
 def format_names(name):
     return name.replace("CUNEIFORM SIGN ","")
+
+def get_braille(text):
+    braille=""
+    print(text)
+    if "[q~*]" in text:
+        text=text.replace("[q~*]","*")
+        if "[q~+]" in text:
+            text=text.replace("[q~+]","+")
+            
+    for char in text:
+        print(char)
+        braille+=braille_object[char.lower()]+"-"
+    return braille
 
 print("enter 1 to generate spreadsheets, enter 2 to add symbols to nvda, or enter 3 to genera te braille table")
 option=int(input("enter one, two, or three"))
@@ -13,6 +30,9 @@ if option == 1:
     name_column=filtered_akkadian["Name"]
     new_name_column=name_column.apply(format_names)
     filtered_akkadian["Name"]=new_name_column
+    braille_column=filtered_akkadian["Braille"]
+    new_braille_column=braille_column.apply(get_braille)
+    filtered_akkadian["Braille"]=new_braille_column
     filtered_akkadian.to_csv("languages/filtered_akkadian.csv")
 elif option==2:
     print("adding symbols to nvda")
@@ -27,6 +47,6 @@ elif option==2:
 elif option==3:
     braille_file=open("utils/brailleconverter.json")
     braille_object=json.load(braille_file)
-    print(braille_object["k"])
+    
 else:
     print("That was not a valid option")
