80a462be37201505958926754f50c674711b15e6
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-06-26 12:21:39 +0300

    Working on UI.

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from nicegui import app, ui
+from nicegui import app, events, ui
 from utils.languages_file import languages
 
 language_list=[]
@@ -18,19 +18,26 @@ def existing_project():
     ui.checkbox("Download Language Spreadsheet")
     ui.button("Next")
 
-@ui.page("/contribute_existing")
-def contribute_existing():
-    ui.button("Go Back",on_click=ui.navigate.back)
-    ui.label("Contribute to an Existing Project")
-
 @ui.page("/create_project")
 def create_project():
     ui.button("Go Back",on_click=ui.navigate.back)
     ui.label("Create New Project")
+    ui.label("Upload your Spreadsheet.")
+    ui.upload(on_upload=handle_file_upload)
+
+def handle_file_upload(e:events.UploadEventArguments):
+    text=e.content.read().decode("utf-8")
+    
+    # Define and open the dialog after processing the file
+    with ui.dialog() as dialog:
+        with ui.card():
+            ui.label("Project Information")
+            ui.label(text[:100])  # Display the first 100 characters for demonstration
+        dialog.open()
+
 
 ui.label("What do you want to do?")
 ui.link("Get Access to Existing Project",existing_project)
-ui.link("Contribute to Existing Project",contribute_existing)
 ui.link("Create a New Project",create_project)
 
 ui.run(native=True,window_size=(400,300),fullscreen=False,title="example")
