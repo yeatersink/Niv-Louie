effb3248ea8b7060b2f46eb5548ffb6de4064068
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-06-27 12:38:26 +0300

    Worked on UI

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
 from nicegui import app, events, ui
 from utils.languages_file import languages
+import pandas as pd
 
-project_name=""
-project_text=""
+project_name=None
+project_text= None
+
 
 language_list=[]
 for language in languages:
@@ -24,25 +26,29 @@ def existing_project():
 
 @ui.page("/create_project")
 def create_project():
-    #create a dialog
-    with ui.dialog() as dialog:
-        with ui.card():
-            ui.label("Project Information")
-            ui.input(label="What is the language code for your language?",)
-
     ui.button("Go Back",on_click=ui.navigate.back)
     ui.label("Create New Project")
     ui.label("Upload your Spreadsheet.")
     ui.upload(on_upload=handle_file_upload)
-    ui.button("Continue",on_click=dialog.open)
+    ui.link("Continue",project_information)
 
 
 def handle_file_upload(e:events.UploadEventArguments):
+    global project_name, project_text
     project_name=e.name
-    project_text=e.content.read().decode("utf-8")
+    project_text=pd.DataFrame(e.content.read().decode("utf-8").splitlines())
+
+@ui.page("/project_information")
+def project_information():
+    global project_name, project_text
+    ui.label("Project Information")            
+    if project_name is not None:
+        ui.input(label="What is the language code for your language?",value=project_name)
+    if project_text is not None:
+        ui.select(label="What column contains the name of the character?",options=project_text.columns.tolist())
 
 ui.label("What do you want to do?")
-ui.link("Get Access to Existing Project",existing_project)
 ui.link("Create a New Project",create_project)
+ui.link("Get Access to Existing Project",existing_project)
 
 ui.run(native=True,window_size=(400,300),fullscreen=False,title="example")
