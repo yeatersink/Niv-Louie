00d82e727cef6f1766c90b714b35b9e60336165a
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-07-12 13:01:48 +0300

    Worked on UI

--- gui.py
+++ gui.py
@@ -1,29 +1,9 @@
 from nicegui import app, events, ui
-import pandas as pd
-import io
-import json
+from utils.project import remove_project, project, languages
 
-project_name=None
-project_text= None
-project_name_column=None
-project_character_column=None
-project_unicode_column=None
-project_type_column=None
-project_braille_column=None
 
-app.native.window_args["resizable"]=False
+app.native.window_args["resizable"]=True
 app.native.start_args["debug"]=True
-
-# Function to load languages from JSON file
-def load_languages():
-    try:
-        with open("utils/languages_file.json", "r", encoding="utf-8") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []  # Return an empty list if the file does not exist
-
-#Loads the languages from the JSON file
-languages = load_languages()
 
 @ui.page("/existing_project")
 def existing_project():
@@ -35,7 +15,7 @@ def existing_project():
     print(language_list)
     ui.button("Go Back",on_click=ui.navigate.back)
     ui.label("Get Access to an Existing Language")
-    ui.select(options=language_list,label="Select a Language",with_input=True,on_change=update_project_name)
+    ui.select(options=language_list,label="Select a Language",with_input=True,on_change=project.update_project_name)
     ui.select(options=["Add Characters to NVDA","Download Files for NVDA","Write Table for Lib Louis","Write Test for Lib Louis"],label="What do you want to do with this Project?",multiple=True)
     with ui.dialog() as confirm_remove_project_dialog, ui.card():
         ui.label("Are you sure you want to completely remove this project?")
@@ -48,72 +28,35 @@ def existing_project():
     ui.button("Next")
 
 
-def remove_project(dialog):
-    global project_name, languages
-    if not project_name ==None:
-        new_languages=filter(check_language_names,languages)
-        languages=list(new_languages)
-        with open("utils/languages_file.json","w",encoding="utf-8") as file:
-            json.dump(languages,file,ensure_ascii=False,indent=4)
-        dialog.close
-        ui.notify("Project Removed",close_button="Ok")
-
-
-def check_language_names(language):
-    global project_name
-    if language["name"]==project_name:
-        return False
-    else:
-        return True
-
-
 @ui.page("/create_project")
 def create_project():
     ui.button("Go Back",on_click=ui.navigate.back)
     ui.label("Create New Project")
     ui.label("Upload your Spreadsheet.")
-    ui.upload(on_upload=handle_file_upload,auto_upload=True)
+    ui.upload(on_upload=project.handle_file_upload,auto_upload=True)
     ui.link("Continue",project_information)
-
-
-def handle_file_upload(e:events.UploadEventArguments):
-    global project_name, project_text
-    project_name=e.name.split(".")[0]
-    content_as_file = io.StringIO(e.content.read().decode("utf-8"))
-    project_text=pd.read_csv(content_as_file)
 
 
 @ui.page("/project_information")
 def project_information():
-    global project_name, project_text
+    global project_name, project_text, project_name_column, project_character_column, project_unicode_column, project_type_column, project_braille_column
+    ui.button("Go Back",on_click=ui.navigate.back)
     ui.label("Project Information")            
-    if project_name is not None:
-        ui.input(label="What is the name of your project?",value=project_name,on_change=update_project_name)
-    if project_text is not None:
-        ui.select(label="What column contains the name of the character?",options=project_text.columns.tolist(),value=project_name_column)
-        ui.select(label="What column contains the character?",options=project_text.columns.tolist(),value=project_character_column)
-        ui.select(label="What column contains the Unicode hex value of the character?",options=project_text.columns.tolist(),value=project_unicode_column)
-        ui.select(label="What column contains the Type of the character?",options=project_text.columns.tolist(),value=project_type_column)
-        ui.select(label="What column contains the Braille character?",options=project_text.columns.tolist(),value=project_braille_column)
-    ui.button("Save Project",on_click=save_project)
-
-def update_project_name(e:events.ValueChangeEventArguments):
-    global project_name
-    project_name=e.value
-
-
-def save_project():
-    global project_name, project_text, project_name_column, project_character_column, project_unicode_column, project_type_column, project_braille_column, languages
-    project_object={"name":project_name,"name_column":project_name_column,"char_column":project_character_column,"braille_column":project_braille_column,"type_column":project_type_column,"unicode_column":project_unicode_column}
-    languages.append(project_object)
-    with open("utils/languages_file.json", "w", encoding="utf-8") as file:
-        json.dump(languages, file, ensure_ascii=False, indent=4)
-    project_text.to_csv("languages/source/"+project_name+".csv",index=False)
-    ui.navigate.to("/existing_project")
+    if project.project_name is not None:
+        ui.input(label="What is the name of your project?",value=project.project_name,on_change=project.update_project_name)
+    if project.project_text is not None:
+        ui.select(label="What column contains the name of the character?",options=project.project_text.columns.tolist(),value=project.project_name_column,on_change=project.update_project_name_column)
+        ui.select(label="What column contains the character?",options=project.project_text.columns.tolist(),value=project.project_character_column,on_change=project.update_project_character_column)
+        ui.select(label="What column contains the Unicode value of the character?",options=project.project_text.columns.tolist(),value=project.project_unicode_column,on_change=project.update_project_unicode_column)
+        ui.select(label="What column contains the Type of the character?",options=project_text.columns.tolist(),value=project_type_column,on_change=project.update_project_type_column)
+        ui.select(label="What column contains the Braille character?",options=project_text.columns.tolist(),value=project_braille_column,on_change=project.update_project_braille_column)
+    ui.button("Save Project",on_click=project.save_project)
 
 
 ui.label("What do you want to do?")
 ui.link("Create a New Project",create_project)
 ui.link("Access Existing Project",existing_project)
+ui.button("Exit",on_click=app.stop)
 
-ui.run(native=True,window_size=(400,300),fullscreen=False,title="example")
+
+ui.run(native=True,window_size=(400,300),fullscreen=False,title="Language Project Manager")
---
+++ utils/project.py
@@ -0,0 +1,107 @@
+from nicegui import app, events, ui
+import io
+import pandas as pd
+import json
+
+
+class Project:
+    def __init__(self):
+        self.project_name = None
+        self.project_text = None
+        self.project_name_column = None
+        self.project_character_column = None
+        self.project_unicode_column = None
+        self.project_type_column = None
+        self.project_braille_column = None
+        self.languages = None
+
+    def update_project_name_column(self, e: events.ValueChangeEventArguments):
+        self.project_name_column = e.value
+
+    def update_project_character_column(self, e: events.ValueChangeEventArguments):
+        self.project_character_column = e.value
+
+    def update_project_unicode_column(self, e: events.ValueChangeEventArguments):
+        self.project_unicode_column = e.value
+
+    def update_project_type_column(self, e: events.ValueChangeEventArguments):
+        self.project_type_column = e.value
+
+    def update_project_braille_column(self, e: events.ValueChangeEventArguments):
+        self.project_braille_column = e.value
+
+    def handle_file_upload(self, e: events.UploadEventArguments):
+        self.project_name = e.name.split(".")[0]
+        content_as_file = io.StringIO(e.content.read().decode("utf-8"))
+        self.project_text = pd.read_csv(content_as_file)
+
+    def save_project(self):
+        if self.project_name is None:
+            ui.notify("Please enter a name for your project.", type="negative")
+        elif self.project_name_column is None:
+            ui.notify("Please select a name column for your project.", type="negative")
+        elif self.project_character_column is None:
+            ui.notify("Please select a character column for your project.", type="negative")
+        elif self.project_unicode_column is None:
+         ui.notify("Please select a Unicode column for your project.", type="negative")
+        elif self.project_type_column is None:
+            ui.notify("Please select a type column for your project.", type="negative")
+        elif self.project_braille_column is None:
+            ui.notify("Please select a braille column for your project.", type="negative")
+
+        for language in languages:
+            if self.project_name.lower() == language.name.lower():
+                ui.notify("A project with that name already exists.",type="negative")
+                return
+
+        project_object= {"name":self.project_name,"name_column":self.project_name_column,"char_column":self.project_character_column,"braille_column":self.project_braille_column,"type_column":self.project_type_column,"unicode_column":self.project_unicode_column}
+        languages.append(project_object)
+        with open("utils/languages_file.json", "w", encoding="utf-8") as file:
+            json.dump(languages, file, ensure_ascii=False, indent=4)
+        project.project_text.to_csv("languages/source/"+project.project_name+".csv",index=False)
+        ui.navigate.to("/existing_project")
+
+        
+project = Project()
+
+# Function to load languages from JSON file
+def load_languages():
+    try:
+        with open("utils/languages_file.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []  # Return an empty list if the file does not exist
+
+#Loads the languages from the JSON file
+languages = load_languages()
+
+def remove_project(dialog):
+    """
+    Remove the project from the languages file and close the dialog.
+    
+    Parameters:
+    dialog: The dialog to be closed.
+    """
+    if not project.project_name ==None:
+        new_languages=filter(check_language_names,languages)
+        languages=list(new_languages)
+        with open("utils/languages_file.json","w",encoding="utf-8") as file:
+            json.dump(languages,file,ensure_ascii=False,indent=4)
+        dialog.close
+        ui.notify("Project Removed",close_button="Ok")
+
+def check_language_names(language):
+    """
+    Check if the language name is the same as the project name.
+    
+    Parameters:
+language: The language to be checked.
+
+Returns:
+True if the language name is not the same as the project name, False otherwise.
+	"""
+    
+    if language["name"]==project.project_name:
+        return False
+    else:
+        return True
